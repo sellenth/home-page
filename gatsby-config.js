4059bf352ae35af7a5438ff5ec9595cfdd9de1e0
sellenth <sellenth@oregonstate.edu>
2020-07-21 21:23:53 -0700

    cleaned up

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
+/* eslint-disable @typescript-eslint/camelcase */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Typescript Jest Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@denningk`,
+    title: `My Page`,
+    description: `My landing page for the internet`,
+    author: `Halston Sellentin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-32x32.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-typescript`,
--- src/components/image.tsx
+++ src/components/image.tsx
@@ -17,7 +17,9 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        placeholderImage: file(
+          relativePath: { eq: "android-chrome-512x512.png" }
+        ) {
           childImageSharp {
             fluid(maxWidth: 300) {
               ...GatsbyImageSharpFluid
